e353d943566cb0b9e7b5af33f58d8fb1335fdfc7
Jairo Ledezma <jairo.arturo.ledezma@gmail.com>
2025-01-21 08:01:27 -0600

    added retries

--- cypress.config.js
+++ cypress.config.js
@@ -4,15 +4,22 @@ module.exports = defineConfig({
   video: true,
   viewportHeight : 1080 , 
   viewportWidth : 1920,
-  defaultCommandTimeout : 20000,
+  defaultCommandTimeout : 5000,
+
+  //retries:1, // most simple way to configure a retry affects all the tests in the suite 
+retries:{
+  runMode:2, //headless will run twice 
+  openMode:0 // with the dashboard will not run
+},
+
   e2e: {
     setupNodeEvents(on, config) {
       const username = process.env.DB_USERNAME
       const password = process.env.PASSWORD
 
-      if(!password){
+      /*if(!password){
         throw new Error(`missing PASSWORD variable`)
-      }
+      }*/
 
       config.env = {username , password}
       return config
